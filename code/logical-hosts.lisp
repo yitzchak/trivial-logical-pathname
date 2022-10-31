95913b71bad8459fe43cf5b0f6a36370bf5d143e
Tarn W. Burton <twburton@gmail.com>
2022-10-31 15:09:42 -0400

    Update for Clasp

--- code/logical-hosts.lisp
+++ code/logical-hosts.lisp
@@ -1,33 +1,37 @@
 (in-package #:trivial-pathname)
 
-(defun logical-hosts ()
+(defun list-all-logical-hosts ()
   #+abcl
-   (loop for key being the hash-key in system:*logical-pathname-translations*
-         collect key)
+  (loop for key being the hash-key in system:*logical-pathname-translations*
+        collect key)
   #+ccl
-   (mapcar #'car ccl::%logical-host-translations%)
-  #+(or clasp ecl)
-   (mapcar #'car (sys:pathname-translations))
+  (mapcar #'car ccl::%logical-host-translations%)
+  #+clasp
+  (ext:list-all-logical-hosts)
   #+cmucl
-   (loop for key being the hash-key in lisp::*logical-hosts*
-         collect key)
+  (loop for key being the hash-key in lisp::*logical-hosts*
+        collect key)
+  #+ecl
+  (mapcar #'car (sys:pathname-translations))
   #+mezzano
-   (mezzano.file-system:list-all-hosts)
+  (mezzano.file-system:list-all-hosts)
   #+sbcl
-   (map 'list #'sb-impl::logical-host-name sb-impl::*logical-hosts*))
+  (map 'list #'sb-impl::logical-host-name sb-impl::*logical-hosts*))
 
 (defun logical-host-p (host)
   #+abcl
-   (system:logical-host-p host)
+  (system:logical-host-p host)
   #+ccl
-   (ccl::logical-host-p host)
-  #+(or clasp ecl)
-   (and (sys:pathname-translations host) t)
+  (ccl::logical-host-p host)
+  #+clasp
+  (ext:logical-host-p host)
   #+cmucl
-   (and (lisp::find-logical-host host nil) t)
+  (and (lisp::find-logical-host host nil) t)
+  #+ecl
+  (and (sys:pathname-translations host) t)
   #+mezzano
-   (and (mezzano.file-system:find-host host nil) t)
+  (and (mezzano.file-system:find-host host nil) t)
   #+sbcl
-   (and (sb-impl::find-logical-host host nil) t)
+  (and (sb-impl::find-logical-host host nil) t)
   #-(or abcl ccl cmucl mezzano sbcl)
-   (typep (make-pathname :host host) 'logical-pathname))
+  (typep (make-pathname :host host) 'logical-pathname))
--- code/packages.lisp
+++ code/packages.lisp
@@ -1,4 +1,4 @@
 (defpackage #:trivial-pathname
   (:use #:common-lisp)
-  (:export #:logical-hosts
+  (:export #:list-all-logical-hosts
            #:logical-host-p))
